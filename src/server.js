1ff1506d8d351e7bdfc8c3ae66306dbe35f0e226
new69 <spalla69@gmail.com>
2019-10-29 22:20:16 -0300

    ADD cors middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import {CONFIG} from './constants';
 import {statusRoutes, webhookRoutes} from './routes';
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json())
+app.use(cors());
 
 // Routes
 app.get('/', statusRoutes.checkStatus);
